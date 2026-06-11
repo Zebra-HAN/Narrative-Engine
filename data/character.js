97a58dfe8d307ddf1ba73934f0b5245fd0b464e1
Zebra-HAN <zebrablackstar@gmail.com>
2026-06-11 17:01:31 +0900

    Update character.js

--- data/character.js
+++ data/character.js
@@ -12,7 +12,7 @@ const CHARACTER_NAV = {
     { id:'race',        label:'종족', icon:'🐉', type:'group'    },
     { id:'job',         label:'직업', img:'images/golem.png',  type:'group'    },
     { id:'personality', label:'성격', icon:'💫'                   },
-    { id:'attribute',   label:'속성', icon:'⚡'   type:'group'  },
+    { id:'attribute',   label:'속성', icon:'⚡'  ,type:'group'  },
     { id:'ability',     label:'능력', icon:'✨'                   },
     { id:'relation',    label:'관계', icon:'🔗'                   },
   ]
